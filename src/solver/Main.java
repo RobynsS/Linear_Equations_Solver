cb71c806835e2f4c5d6c38d8dd1e81cc71533a2f
Steven Robyns <robynssteven@gmail.com>
2020-05-03 22:01:03 +0200

    Initial commit

---
+++ src/solver/Main.java
@@ -0,0 +1,19 @@
+package solver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Read input file and store elements in Matrix object
+        double[][] inputs = Reader.readElementsFromInput(args[1]);
+        Matrix matrix = new Matrix(inputs);
+
+        // Solve the linear equations
+        Solver solver = new Solver(matrix);
+        solver.solve();
+        double[] solution = solver.getSolution();
+
+        //Write solution to File
+        Writer.readSolutionToOutput(args[3], solution);
+    }
+}
